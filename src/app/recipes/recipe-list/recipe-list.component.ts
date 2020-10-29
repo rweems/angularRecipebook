08d5f3f891eb29e26d77ac25187fd406a0a25977
Rickey <weems505@gmail.com>
2020-10-29 15:41:49 -0500

    Update recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+
+  recipes: Recipe[] = [
+    new Recipe('Baked Ziti',
+      'Classic Italian American comfort food of pasta baked with sausage',
+      'https://www.spendwithpennies.com/wp-content/uploads/2019/03/Baked-Ziti-SpendWithPennies-10.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
