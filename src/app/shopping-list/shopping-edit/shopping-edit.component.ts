82faae87a3437f7eb2524dd1733024f27f30e5f9
Rickey <weems505@gmail.com>
2020-11-03 15:05:55 -0600

    Added functionality to add button

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,7 +10,20 @@ export class ShoppingEditComponent implements OnInit {
 
   constructor() { }
 
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+
+  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+
   ngOnInit() {
   }
 
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmt = this.amountInputRef.nativeElement.value;
+
+    const newIngredient = new Ingredient(ingName, ingAmt);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
